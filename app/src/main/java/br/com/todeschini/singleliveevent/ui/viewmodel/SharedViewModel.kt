f1c9d55625feed94d5348e24b062ef1be7041ed5
Bruno Todeschini Oliveira <btodesch@SAO-FVFWXAH8HV29.local>
2022-02-21 12:27:17 -0300

    removing dependencie

--- app/src/main/java/br/com/todeschini/singleliveevent/ui/viewmodel/SharedViewModel.kt
+++ app/src/main/java/br/com/todeschini/singleliveevent/ui/viewmodel/SharedViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import br.com.todeschini.singleliveevent.event.SingleLiveEvent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
